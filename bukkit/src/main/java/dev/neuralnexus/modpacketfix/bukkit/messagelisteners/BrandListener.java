ddda6e504d1292b0dc25f795a2a5c54f4acff18b
OooooorangeClover <121479279+ERroooooR@users.noreply.github.com>
2024-05-05 01:40:31 +0800

    一视同仁.jpg

--- bukkit/src/main/java/dev/neuralnexus/modpacketfix/bukkit/messagelisteners/BrandListener.java
+++ bukkit/src/main/java/dev/neuralnexus/modpacketfix/bukkit/messagelisteners/BrandListener.java
@@ -2,31 +2,29 @@ package dev.neuralnexus.modpacketfix.bukkit.messagelisteners;
 
 import dev.neuralnexus.modpacketfix.bukkit.BukkitModPacketFixPlugin;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 import org.jetbrains.annotations.NotNull;
 
-/**
- * Listens for the brand plugin message and registers the player as a forge user if they are using forge
- */
-public class BrandListener implements PluginMessageListener {
+public class BrandListener implements PluginMessageListener, Listener {
     private final BukkitModPacketFixPlugin plugin;
+
     public BrandListener(BukkitModPacketFixPlugin plugin) {
         this.plugin = plugin;
     }
 
     @Override
     public void onPluginMessageReceived(@NotNull String channel, @NotNull Player player, @NotNull byte[] message) {
-        if (channel.equals("MC|Brand") || channel.equals("minecraft:brand")) {
-            String clientName = new String(message).trim().toLowerCase();
-            if (clientName.contains("forge")) {
-                plugin.addForgeUser(player);
-                plugin.getLogger().info("Registered " + player.getName() + " as a forge user");
-            } else {
-                if (plugin.isForgeUser(player)) {
-                    plugin.removeForgeUser(player);
-                    plugin.getLogger().info("Unregistered " + player.getName() + " as a forge user");
-                }
-            }
-        }
+        // 这里可以保留原有的 onPluginMessageReceived 方法逻辑
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        // 在玩家加入时将他们添加到 Forge 用户列表
+        plugin.addForgeUser(player);
+        plugin.getLogger().info("Registered " + player.getName() + " as a forge user");
     }
 }
